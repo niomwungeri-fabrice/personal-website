7c9e29f497440b486878cb8ddca2a2b1ff1c799a
Fabrice Niyomwungeri <niomwungeri.fabrice@gmail.com>
2025-01-03 16:47:17 -0700

    reformat

--- api/src/main/java/io/lynx/api/controllers/ContactMessageController.java
+++ api/src/main/java/io/lynx/api/controllers/ContactMessageController.java
@@ -8,9 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-
 @RequestMapping("/api/contacts")
-
 public class ContactMessageController {
 
     private final ContactMessageService contactMessageService;
